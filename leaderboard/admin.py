22ffd9fdb7157af2538712427ea9f65249c5f769
Christian Federmann <chrife@microsoft.com>
2019-05-30 08:44:07 -0700

    Added TestSet ModelAdmin

--- leaderboard/admin.py
+++ leaderboard/admin.py
@@ -3,12 +3,20 @@ Project OCELoT: Open, Competitive Evaluation Leaderboard of Translations
 """
 from django.contrib import admin
 
-from leaderboard.models import Language
+from leaderboard.models import Language, TestSet
 
 
 class LanguageAdmin(admin.ModelAdmin):
     """Model admin for Language objects."""
+
     fields = ['code', 'name']
 
 
+class TestSetAdmin(admin.ModelAdmin):
+    """Model admin for TestSet objects."""
+
+    fields = ['name']
+
+
 admin.site.register(Language, LanguageAdmin)
+admin.site.register(TestSet, TestSetAdmin)
