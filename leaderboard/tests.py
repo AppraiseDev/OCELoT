ed39a8bd3b68ad00d1aa3114ab9794fc44724cbd
Christian Federmann <chrife@microsoft.com>
2019-08-28 07:09:48 -0700

    Pylint cleanup

--- leaderboard/tests.py
+++ leaderboard/tests.py
@@ -1,6 +1,7 @@
 """
 Project OCELoT: Open, Competitive Evaluation Leaderboard of Translations
 """
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 
@@ -28,9 +29,41 @@ class LeaderboardTests(TestCase):
         """Checks that frontpage retrieve list of test sets."""
         from leaderboard.models import Language, TestSet
 
+        _msg = 'Need to implement test for Language and TestSet objects'
+        with self.assertRaisesMessage(NotImplementedError, _msg):
+            raise NotImplementedError(_msg)
+
     def test_testset_model_has_name_and_json_data(self):
         """Checks that TestSet model has name and JSON data."""
-        from .models import TestSet
-        a = TestSet(name='foo', json_data='bar')
-        self.assertEqual(a.name, 'foo')
-        self.assertEqual(a.json_data, 'bar')
+        from leaderboard.models import TestSet
+
+        _good = TestSet(name='foo', json_data='bar')
+        self.assertEqual(_good.name, 'foo')
+        self.assertEqual(_good.json_data, 'bar')
+
+    def test_testset_needs_both_name_and_json_data(self):
+        """Checks that TestSet model needs both name and JSON data."""
+        from leaderboard.models import TestSet
+
+        _bad = TestSet()
+
+        _msg = (
+            "{'name': ['This field cannot be blank.'], "
+            "'json_data': ['This field cannot be blank.']}"
+        )
+        with self.assertRaisesMessage(ValidationError, _msg):
+            _bad.full_clean()
+
+        _bad.name = 'foo'
+        _msg = "{'json_data': ['This field cannot be blank.']}"
+        with self.assertRaisesMessage(ValidationError, _msg):
+            _bad.full_clean()
+
+        _bad = TestSet()
+        _bad.json_data = 'bar'
+        _msg = "{'name': ['This field cannot be blank.']}"
+        with self.assertRaisesMessage(ValidationError, _msg):
+            _bad.full_clean()
+
+        _good = TestSet(name='foo', json_data='bar')
+        _good.full_clean()
