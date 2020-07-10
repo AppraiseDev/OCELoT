149739154af494f0c4a16ab92c8219144cf03a37
Christian Federmann <cfedermann@gmail.com>
2020-07-10 15:42:07 -0700

    Fixed submission form to only show active test sets.

--- leaderboard/forms.py
+++ leaderboard/forms.py
@@ -7,6 +7,7 @@ from leaderboard.models import MAX_NAME_LENGTH
 from leaderboard.models import MAX_TOKEN_LENGTH
 from leaderboard.models import Submission
 from leaderboard.models import Team
+from leaderboard.models import TestSet
 from leaderboard.models import validate_team_name
 from leaderboard.models import validate_token
 
@@ -39,6 +40,12 @@ class SigninForm(forms.Form):
 
 class SubmissionForm(forms.ModelForm):
     """Form used for submission view."""
+
+    test_set = forms.ModelChoiceField(
+        queryset=TestSet.objects.filter(  # pylint: disable=no-member
+            is_active=True
+        )
+    )
 
     class Meta:  # pylint: disable=too-few-public-methods,missing-docstring
         model = Submission
